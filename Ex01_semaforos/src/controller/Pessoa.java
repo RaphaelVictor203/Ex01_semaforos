18437692f085b1b2f60b29803b066b4ee2ef18be
RaphaelVictor203 <RaphaelVictor203@github.com>
2018-09-03 21:48:13 -0300

    Alteração das classe Main e Pessoa

--- Ex01_semaforos/src/controller/Pessoa.java
+++ Ex01_semaforos/src/controller/Pessoa.java
@@ -1,10 +1,146 @@
 package controller;
 
+import java.awt.Rectangle;
+import java.util.concurrent.Semaphore;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class Pessoa extends Thread{
 	
-	public Pessoa() {
+	private int movX, movY;
+	private String[] frames;
+	private int frameAtual = 0;
+	private JLabel porta;
+	private String[] framesPorta;
+	private JLabel label;
+	private Semaphore semaforo;
 	
+	public Pessoa(int movX, int movY, String[] frames, JLabel porta, String[] framesPorta, JLabel label, Semaphore semaforo) {
+		this.movX = movX;
+		this.movY = movY;
+		this.frames = frames;
+		this.porta = porta;
+		this.label = label;
+		this.semaforo = semaforo;
+		this.framesPorta = framesPorta;
 	}
 	
-	public void run() {};
+	public void run() {
+		correr();
+	};
+	
+	private void correr() {
+		int vl = (int)((Math.random()*3) + 4);
+		if(movX != 0) {
+			if(movX < 0) {
+				while(label.getBounds().x > (porta.getBounds().x + 100)) {
+					mover(vl*(-1), 0);
+				}
+			}else {
+				while(label.getBounds().x < (porta.getBounds().x - 100)) {
+					mover(vl, 0);
+				}
+			}
+		}else 
+		if(movY != 0){
+			if(movY < 0) {
+				while(label.getBounds().y < (porta.getBounds().y - 50)) {
+					mover(0, vl);
+				}
+			}else {
+				while(label.getBounds().y > (porta.getBounds().y + 50)) {
+					mover(0, vl*(-1));
+				}
+			}
+		}
+		try {
+			semaforo.acquire();
+			andarParaPorta();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			semaforo.release();
+		}
+	}
+	
+	private void mover(int x, int y) {
+		label.setLocation(label.getBounds().x + x, label.getBounds().y + y);
+		try {
+			if(frameAtual < (frames.length-1)) {
+				frameAtual++;
+			}else {
+				frameAtual = 0;
+			}
+			label.setIcon(new ImageIcon(frames[frameAtual]));
+			sleep(150);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void andarParaPorta() {
+		int vl = (int)((Math.random()*3) + 4);
+		if(movY != 0) {
+			if(movY < 0) {
+				while(label.getBounds().y < porta.getBounds().y) {
+					mover(0, vl);
+				}
+			}else {
+				while(label.getBounds().y > (porta.getBounds().y)+25) {
+					mover(0, vl*(-1));
+				}
+			}
+		}else 
+		if(movX != 0){
+			if(movX < 0) {
+				while(label.getBounds().x > porta.getBounds().x) {
+					mover(vl*(-1), 0);
+				}
+			}else {
+				while(label.getBounds().x < porta.getBounds().x) {
+					mover(vl, 0);
+				}
+			}
+		}
+		try {
+			sleep(((int)(Math.random()*2)+1)*1000);
+			abrirPorta();
+			label.setIcon(new ImageIcon(""));
+			fecharPorta();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void abrirPorta() {
+		for(int i=0; i<framesPorta.length; i++) {
+			ImageIcon imgPorta = new ImageIcon(framesPorta[i]);
+			imgPorta.setImage(imgPorta.getImage().getScaledInstance(37, 39, 100));
+			porta.setIcon(imgPorta);
+			try {
+				sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void fecharPorta() {
+		for(int i=framesPorta.length-1; i>=0; i--) {
+			ImageIcon imgPorta = new ImageIcon(framesPorta[i]);
+			imgPorta.setImage(imgPorta.getImage().getScaledInstance(37, 39, 100));
+			porta.setIcon(imgPorta);
+			try {
+				sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- Ex01_semaforos/src/view/Main.java
+++ Ex01_semaforos/src/view/Main.java
@@ -1,11 +1,20 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.Pessoa;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.util.concurrent.Semaphore;
 
 public class Main extends JFrame {
 
@@ -35,8 +44,52 @@ public class Main extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		ImageIcon imgPorta = new ImageIcon("personagens\\porta_0.png");
+		imgPorta.setImage(imgPorta.getImage().getScaledInstance(37, 39, 100));
+		
+		JLabel lblPessoa3 = new JLabel();
+		lblPessoa3.setBounds(185, 239, 56, 31);
+		contentPane.add(lblPessoa3);
+		
+		JLabel lblPessoa4 = new JLabel();
+		lblPessoa4.setBounds(12, 132, 56, 31);
+		contentPane.add(lblPessoa4);
+		
+		JLabel lblPessoa2 = new JLabel();
+		lblPessoa2.setBounds(374, 132, 56, 31);
+		contentPane.add(lblPessoa2);
+		
+		JLabel lblPorta = new JLabel(imgPorta);
+		lblPorta.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblPorta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPorta.setBounds(185, 110, 37, 39);
+		contentPane.add(lblPorta);
+		
+		JLabel lblPessoa1 = new JLabel();
+		lblPessoa1.setBounds(185, 13, 56, 31);
+		contentPane.add(lblPessoa1);
+		
+		Semaphore semaphore = new Semaphore(1);
+		
+		String[] framesPorta = new String[] {"personagens\\porta_0.png","personagens\\porta_1.png", "personagens\\porta_2.png", "personagens\\porta_3.png"};
+		
+		String[] frames = new String[] {"personagens\\personagem1_0.png", "personagens\\personagem1_1.png", "personagens\\personagem1_2.png"};
+		Pessoa pessoa1 = new Pessoa(0, (-1), frames, lblPorta, framesPorta, lblPessoa1, semaphore);
+		pessoa1.start();
+		
+		frames = new String[] {"personagens\\personagem2_0.png", "personagens\\personagem2_1.png", "personagens\\personagem2_2.png"};
+		Pessoa pessoa2 = new Pessoa((-1), 0, frames, lblPorta, framesPorta, lblPessoa2, semaphore);
+		pessoa2.start();
+		
+		frames = new String[] {"personagens\\personagem3_0.png", "personagens\\personagem3_1.png", "personagens\\personagem3_2.png"};
+		Pessoa pessoa3 = new Pessoa(0, 1, frames, lblPorta, framesPorta, lblPessoa3, semaphore);
+		pessoa3.start();
+		
+		frames = new String[] {"personagens\\personagem4_0.png", "personagens\\personagem4_1.png", "personagens\\personagem4_2.png"};
+		Pessoa pessoa4 = new Pessoa(1, 0, frames, lblPorta, framesPorta, lblPessoa4, semaphore);
+		pessoa4.start();
 	}
-
 }
